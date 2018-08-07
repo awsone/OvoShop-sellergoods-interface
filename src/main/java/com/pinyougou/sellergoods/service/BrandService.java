eded5d14599b9254c7ccd8b11b6230e96470db92
ovo <1243121947@qq.com>
2018-08-07 17:44:40 +0800

    1.8.7

---
+++ src/main/java/com/pinyougou/sellergoods/service/BrandService.java
@@ -0,0 +1,16 @@
+package com.pinyougou.sellergoods.service;
+
+import java.util.List;
+
+import com.pinyougou.pojo.TbBrand;
+
+/**
+ * 品牌
+ * @author 大白猫😀😀😀小地瓜
+ *
+ */
+public interface BrandService {
+
+	
+	public List<TbBrand> findAll();
+}
