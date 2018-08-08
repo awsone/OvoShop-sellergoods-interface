b774dd7c7ed4ecebe005aa0dc73e1cc13a7b88ab
ovo <1243121947@qq.com>
2018-08-08 21:34:06 +0800

    2.8.8

--- src/main/java/com/pinyougou/sellergoods/service/BrandService.java
+++ src/main/java/com/pinyougou/sellergoods/service/BrandService.java
@@ -4,6 +4,8 @@ import java.util.List;
 
 import com.pinyougou.pojo.TbBrand;
 
+import entity.PageResult;
+
 /**
  * 品牌
  * @author 大白猫😀😀😀小地瓜
@@ -11,6 +13,57 @@ import com.pinyougou.pojo.TbBrand;
  */
 public interface BrandService {
 
-	
+
 	public List<TbBrand> findAll();
+
+
+	/**
+	 * 品牌分页
+	 * @param pageNum  当前页面
+	 * @param pageSize  页面大小
+	 * @return
+	 */
+	public PageResult findPage(int pageNum,int pageSize);
+
+	/**
+	 * 添加品牌
+	 * @param tbBrand
+	 */
+	public void add(TbBrand tbBrand);
+	
+	/**
+	 * 根据id查询实体
+	 * @param id
+	 * @return
+	 */
+	public TbBrand findOne(Long id);
+	
+	
+	/**
+	 * 修改结果存入
+	 * @param tbBrand
+	 */
+	public void update(TbBrand tbBrand);
+	
+	/**
+	 * 批量删除
+	 * @param ids
+	 */
+	public void delete(Long[] ids);
+	
+	
+	
+	/**
+	 * 按照条件查询
+	 * @param brand
+	 * @param pageNum
+	 * @param pageSize
+	 * @return
+	 */
+	public PageResult findPage(TbBrand brand,int pageNum,int pageSize);
+
+	
+	
+	
+	
 }
